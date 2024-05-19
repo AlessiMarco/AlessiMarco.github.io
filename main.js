2ee1d60ee874d2bbe177ff5692c42e38bb2cea87
Marco Alessi <alessi.marco16@gmail.com>
2024-05-19 20:59:47 +0200

    Commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Application } from '@splinetool/runtime';
+import { Application } from 'https://cdn.jsdelivr.net/npm/@splinetool/runtime@0.9.209/build/spline.module.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('canvas3d');
