045355c3aae4524e34132e9f45f91ab5c06974dd
Marco Alessi <alessi.marco16@gmail.com>
2024-05-24 20:27:11 +0200

    Changes

---
+++ hero_desktop.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    const canvas = document.getElementById('canvas3ddesktop');
+    const { Application } = await import('./DesktopHero/runtime.js');
+    const app = new Application(canvas);
+    app.load('./DesktopHero/scene.splinecode');
+
+});
